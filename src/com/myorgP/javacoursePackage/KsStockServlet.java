2696c90e8e193bd15bfbaf516c919e279786ce26
shifrinkatia <shifrinkatia@gmail.com>
2015-04-02 15:19:37 +0300

    version 2

--- src/com/myorgP/javacoursePackage/KsStockServlet.java
+++ src/com/myorgP/javacoursePackage/KsStockServlet.java
@@ -5,7 +5,19 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class KsStockServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		
+		//variable declaration and assignment
+		int num1=4, num2=3, num3=7;
+		
+		//calculation
+		int result = (num1+num2)*num3;
+		String resultStr = new String ("<h1> Result of "+" ("+num1+"+"+num2+")"+"*"+num3+"="+result+"</h1>");
+		
+		
+		
+		resp.getWriter().println( resultStr );
+		
+
 	}
 }
